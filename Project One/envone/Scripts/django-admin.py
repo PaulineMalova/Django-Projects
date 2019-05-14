34d7b50f480d14aec50120a7c1761620a5418517
paulinemalova <perleebrown@gmail.com>
2019-05-14 20:08:15 +0300

    Creating a virtual environment activating it installing django creating a django project and creating its IP address on the command prompt

---
+++ Project One/envone/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\student\documents\class for python\django-projects\envone\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
